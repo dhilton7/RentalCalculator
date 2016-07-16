56627ea9f8c36563ccdf3e95db79a3f933b10be0
Dan Hilton <dhilton@missiondata.com>
2016-07-16 11:06:38 -0400

    touch up UI on new deal form

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -11,11 +11,31 @@ class DealsController < ApplicationController
 		if @deal.save
 			redirect_to deal_path(@deal)
 		else
-
+			render :new
 		end
 	end
 
 	def show
+	end
+
+	def index
+		@deals = Deal.all
+	end
+
+	def edit
+	end
+
+	def update
+		if @deal.update deal_params
+			redirect_to deal_path(@deal)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@deal.destroy
+		redirect_to deals_path, notice: "Deal was successfully deleted."
 	end
 
 	private
