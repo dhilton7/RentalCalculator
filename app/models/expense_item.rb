30a7e59d103d1a4227ced7ab385d23c2d4bd3a2e
Dan Hilton <dhilton@missiondata.com>
2016-12-28 21:49:05 -0500

    add custom getter and setter on amount for income and expense items

--- app/models/expense_item.rb
+++ app/models/expense_item.rb
@@ -6,4 +6,12 @@ class ExpenseItem < ActiveRecord::Base
 	# Validations
 	validates :name, :amount, presence: true
 
+	def amount=(value)
+		super value.to_f * 100
+	end
+
+	def amount
+		super.to_i / 100.00
+	end
+
 end
